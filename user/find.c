70cd267b069a2a07893dd4aef81ee61ddb9d27df
Bowser1704 <bowser1704@icloud.com>
2020-09-24 10:51:29 +0800

    end lab-1

--- user/find.c
+++ user/find.c
@@ -3,58 +3,9 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-char* fmtname(char*);
-
-struct dirent de;
-struct stat st;
-
-char* concat(const char *s1, const char *s2)
+int
+wanted_file(char *path, char *name)
 {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
-}
-
-void find(char *path, char *name) {
-    int fd;
-    if ((fd = open(path, 0)) < 0) {
-        fprintf(2, "ls: cannot open %s\n", path);
-        return;
-    }
-
-    if (fstat(fd, &st) < 0) {
-        fprintf(2, "ls: cannot stat %s\n", path);
-        close(fd);
-        return;
-    }
-
-    // tow much string / char pointer. it makes me confused.
-    switch(st.type) {
-        case T_FILE:
-            if (strcmp(fmtname(path), name) == 0)
-                printf("%s\n", concat(concat(path, "/"), name));
-            break;
-        case T_DIR:
-            break;
-    }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(2, "usage: find path target-name\n");
-        exit(1);
-    }
-    //char prefix[512];
-    find(argv[1], argv[2]);
-    exit(0);
-}
-
-char*
-fmtname(char *path)
-{
-  static char buf[DIRSIZ+1];
   char *p;
 
   // Find first character after last slash.
@@ -62,10 +13,62 @@ fmtname(char *path)
     ;
   p++;
 
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-  return buf;
+  if(strcmp(p, name) == 0) return 1; else return 0;
+}
+
+
+void find(char* path, char* name){
+  char buf[64], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_FILE:
+    if (wanted_file(path, name))
+        printf("%s\n", path);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      find(buf, name);
+    }
+    break;
+  }
+  close(fd);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+  if(argc < 3){
+    exit(1);
+  }
+  find(argv[1], argv[2]);
+  exit(0);
 }
