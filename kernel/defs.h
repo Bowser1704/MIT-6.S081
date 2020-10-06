75d26d7ad039a4af77e1e6e3328db1f784a3e1e8
Bowser1704 <bowser1704@icloud.com>
2020-10-06 15:42:19 +0800

    add lab3 pgtbl

--- kernel/defs.h
+++ kernel/defs.h
@@ -158,6 +158,8 @@ void            uartputc_sync(int);
 int             uartgetc(void);
 
 // vm.c
+void            vmprint(pagetable_t);
+// default
 void            kvminit(void);
 void            kvminithart(void);
 uint64          kvmpa(uint64);
--- kernel/vm.c
+++ kernel/vm.c
@@ -16,6 +16,40 @@ extern char etext[];  // kernel.ld sets this to end of kernel code.
 extern char trampoline[]; // trampoline.S
 
 /*
+ * prints the content of page tables
+ */
+void
+vmprint(pagetable_t pd_l2)
+{
+  printf("page table %p\n", pd_l2);
+  int i, j, k;
+  for(i = 0; i < PAGETABLE_READ; i++) {
+    pagetable_t pd_l1 = (pagetable_t)PTE2PA(pd_l2[i]);
+    // PTE is valid or not
+    if (pd_l1 && PTE_V) {
+      printf("..");
+      printf("%d: pte %p pa %p\n", i, pd_l2[i], pd_l1);
+      for(j = 0; j < PAGETABLE_READ; j++) {
+        pagetable_t pd_l0 = (pagetable_t)PTE2PA(pd_l1[j]);
+        // PTE is valid or not
+        if (pd_l0 && PTE_V) {
+          printf(".. ..");  
+          printf("%d: pte %p pa %p\n", j, pd_l1[j], pd_l0);
+          for(k = 0; k < PAGETABLE_READ; k++) {
+            pagetable_t pte = (pagetable_t)PTE2PA(pd_l0[k]);
+            // PTE is valid or not
+            if (pte && PTE_V) {
+              printf(".. .. ..");  
+              printf("%d: pte %p pa %p\n", k, pd_l0[k], pte);
+            }
+          }
+        } 
+      }
+    }
+  } 
+}
+
+/*
  * create a direct-map page table for the kernel.
  */
 void
